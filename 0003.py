6448a7b2a09c9863dc7427ef01422fdbbd6148b1
Cleisson <cleissonconstc@gmail.com>
2024-03-19 22:33:16 -0300

    refacor

--- 0003.py
+++ 0003.py
@@ -1,8 +1,13 @@
+# https://projecteuler.net/problem=3
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143?
+
+
 def get_largest_prime_factor(n):
     largest_prime = 0
     i = 2
 
-    while i*i <= n:
+    while i * i <= n:
         while n % i == 0:
             largest_prime = i
             n //= i
@@ -13,9 +18,11 @@ def get_largest_prime_factor(n):
 
     return largest_prime
 
+
 def main():
     number = 600851475143
-    print(get_largest_prime_factor(number)) # 6857
+    print(get_largest_prime_factor(number))  # 6857
+
 
 if __name__ == "__main__":
     main()
