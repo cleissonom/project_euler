79abe8e08bab1ecb6379030b3701c466609f2d0a
Cleisson <cleissonconstc@gmail.com>
2024-07-19 03:11:25 -0300

    removing unused lib

--- 0009.py
+++ 0009.py
@@ -5,8 +5,6 @@
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
 
-import math
-
 from helpers import measure_time
 
 
--- 0010.py
+++ 0010.py
@@ -2,8 +2,6 @@
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 # Find the sum of all the primes below two million.
 
-import math
-
 from helpers import measure_time
 
 
